f812d708781300ef0a2a46154ed9c360a1555ee3
Onlu <onlu.contact@gmail.com>
2025-05-27 23:48:18 +0300

    - added vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,27 @@
 import MuiProvider from '@/components/MuiProvider.jsx';
 import ClientLayout from '@/components/ClientLayout.jsx';
 import './globals.css';
+import {Analytics} from '@vercel/analytics/next';
 
 export const metadata = {
-  title: 'Mr. Cut',
-  description: 'Professional barber shop',
+    title: 'Mr. Cut',
+    description: 'Professional barber shop',
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" dir={"rtl"}>
-      <head title={"Mr Cut"}>
-        <meta name="emotion-insertion-point" content="" />
-      </head>
-      <body>
+export default function RootLayout({children}) {
+    return (
+        <html lang="en" dir={"rtl"}>
+        <head title={"Mr Cut"}>
+            <meta name="emotion-insertion-point" content=""/>
+        </head>
+        <body>
         <MuiProvider>
-          <ClientLayout>
-            {children}
-          </ClientLayout>
+            <Analytics/>
+            <ClientLayout>
+                {children}
+            </ClientLayout>
         </MuiProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 } 
